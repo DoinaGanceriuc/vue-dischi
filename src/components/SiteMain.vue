cd91bbe7bd3ba7ff404c56589bda6bc2b0100aa7
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-15 14:57:57 +0100

    complete details SiteMain component

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,19 +1,29 @@
 <template>
   <main id="site_main">
     <div class="container">
-        <div class="row row-cols-md-6 gx-5 py-5 justify-content-center">
-  <div class="col py-3" v-for="poster in posters" :key="poster.id">
-    <div class="card h-100">
-      <img :src="poster.poster" class="card-img-top" width="50" :alt="poster.author">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div class="row row-cols-md-6 gx-5 py-5 justify-content-center">
+        <div class="col py-3" v-for="poster in posters" :key="poster.id">
+          <div class="card h-100 align-items-center">
+            <img
+              :src="poster.poster"
+              class="card-img-top"
+              :alt="poster.author"
+            />
+            <div class="card-body text-center">
+              <h5 class="card-title text-white">
+                {{ poster.title.toUpperCase() }}
+              </h5>
+              <p class="card-text text-muted m-0">{{ poster.author }}</p>
+              <p class="card-text text-muted">{{ poster.year }}</p>
+            </div>
+          </div>
+            <!-- /.card -->
+        </div>
+          <!-- /.col -->
       </div>
+        <!-- /.row -->
     </div>
-  </div>
-</div>
-    </div>
-
+    <!-- /.container -->
   </main>
   <!-- /#site_main -->
 </template>
@@ -25,7 +35,8 @@ export default {
   data () {
     return {
       posters: [],
-      API_URL: 'https://flynn.boolean.careers/exercises/api/array/music'
+      API_URL: 'https://flynn.boolean.careers/exercises/api/array/music',
+      loading: true
     }
   },
   mounted () {
@@ -46,7 +57,16 @@ export default {
 
 <style lang="scss">
 main {
-  background-color: #1E2D3B;
-}
+  background-color: #1e2d3b;
+  height: calc(100vh - 100px);
+  .card {
+    background-color: #2e3a46;
 
+    .card-img-top {
+      width: 130px;
+      height: 130px;
+      padding-top: 1rem;
+    }
+  }
+}
 </style>
