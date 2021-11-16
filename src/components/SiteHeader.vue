07f53e0c630c6c7e2297503c42d2784cad62131c
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-16 12:43:27 +0100

    add select

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,7 +1,18 @@
 <template>
   <header id="site_header">
-    <div class="logo text-start align-middle container">
-       <img width="50" src="../assets/spotify-logo.png" alt="">
+    <div class="container d-flex justify-content-between align-items-center">
+      <div class="logo text-start align-middle">
+        <img width="50" src="../assets/spotify-logo.png" alt="" />
+      </div>
+      <div>
+        <select class="form-select" aria-label="">
+          <option selected>Select genre</option>
+          <option value="Rock">Rock</option>
+          <option value="Pop">Pop</option>
+          <option value="Jazz">Jazz</option>
+          <option value="Metal">Metal</option>
+        </select>
+      </div>
     </div>
     <!-- /.container -->
   </header>
@@ -9,13 +20,11 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="scss">
-@import '../assets/scss/variables.scss';
+@import "../assets/scss/variables.scss";
 header {
   height: 100px;
   background-color: $bg_dark_top;
@@ -23,5 +32,4 @@ header {
     line-height: 100px;
   }
 }
-
 </style>
